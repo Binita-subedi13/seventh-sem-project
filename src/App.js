7cb39ef804ddb6465253173b4de45f0c3d095589
Binita-subedi13 <sbinita0303@gmail.com>
2023-02-16 15:45:39 +0545

    no change

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Profile from "./components/Body section/Profile";
 import Demo from "./components/Content/Demo";
 import Events from "./components/Content/Events"; 
 import Slider from "./components/Content/Slider";
+import Banner from "./components/Content/Banner";
 function App() {
   
   return (
@@ -10,6 +11,7 @@ function App() {
 <div><Slider/>
 <Profile/>
 <Events></Events>
+<Banner/>
 <Demo/>
 </div>
 
